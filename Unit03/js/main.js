0f04fe1c92a1e694eaf8d8509a787cfcd29bdc60
Sarah <srhmendez@gmail.com>
2020-02-16 23:56:49 -0700

    Completed Guessing Game

--- Unit03/js/main.js
+++ Unit03/js/main.js
@@ -19,6 +19,7 @@ function evalGuess() {
 
     if (gamerGuess == correctNumber) {
         feedback.innerText = 'You are a winner!!!'
+        giveAward()
     }
     else if (gamerGuess > correctNumber && gamerGuess < 16) {
         feedback.innerText = 'Too high, try again.'
@@ -28,9 +29,40 @@ function evalGuess() {
     }
     else {
         feedback.innerText = 'Please Choose a number between 1 & 15 and try again.'
+        totalGuesses -= 1
     }
     //End of if else statement
 
     const attempts = document.querySelector('#attempts')
     attempts.innerText = `${totalGuesses}`
 }// End of evalGuess Function
+
+function giveAward() {
+    let imgPath = 'images/yellowribbon.png'
+    switch(totalGuesses) {
+        case 1:
+        case 2:
+        case 3: 
+            imgPath = 'images/blueribbon.png'
+            console.log('Blue Ribbon for you')
+            break;
+        case 4:
+        case 5:
+        case 6: 
+            imgPath = 'images/redribbon.png'
+            console.log('Red Ribbon for you')
+            break;
+        case 7:
+            console.log('Yellow Ribbon for you')
+            break;
+    }
+
+    let awardImage = document.createElement('img') //Creates a <img> element in the 
+    awardImage.setAttribute('src', imgPath)
+
+    const ribbon = document.querySelector('#ribbon') //assigns ribbon to #ribbon div in DOM
+    ribbon.appendChild(awardImage)
+
+    
+    
+}
